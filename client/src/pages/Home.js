257be95cfe07f147901f4848c3d5cd15c8cda64e
Jehyun <congmul@hotmail.com>
2021-08-12 11:02:40 -0700

    Client Side

---
+++ client/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_BOOKS } from '../utils/queries';
+import BookList from '../components/BookList';
+
+const Home = () => {
+    const { loading, data } = useQuery(QUERY_BOOKS);
+
+    // Optional Operator - ?
+    const books = data?.books || [];
+
+    return(
+        <main>
+            <div>
+                {loading? <div>Loading...</div> : <BookList books={books} />}
+            </div>
+        </main>
+    )
+};
+
+export default Home;
